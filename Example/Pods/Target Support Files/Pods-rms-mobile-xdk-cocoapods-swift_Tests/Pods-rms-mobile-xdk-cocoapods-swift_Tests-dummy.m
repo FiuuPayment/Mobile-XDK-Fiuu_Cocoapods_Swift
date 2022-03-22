bffec339c96bd9c56f388a6bc4a052753cafc8c2
Abd Qayyum <qayyum.ishak@razer.com>
2022-03-22 14:50:41 +0800

    project creation

---
+++ Example/Pods/Target Support Files/Pods-rms-mobile-xdk-cocoapods-swift_Tests/Pods-rms-mobile-xdk-cocoapods-swift_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rms_mobile_xdk_cocoapods_swift_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_rms_mobile_xdk_cocoapods_swift_Tests
+@end
