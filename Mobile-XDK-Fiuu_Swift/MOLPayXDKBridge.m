8f844fc7ca71da5552b6ae9728cff463c46f1ddb
Hanif <hanif.hasan@razer.com>
2024-05-23 15:59:11 +0800

    implement cocopod

---
+++ Mobile-XDK-Fiuu_Swift/MOLPayXDKBridge.m
@@ -0,0 +1,14 @@
+//
+//  MOLPayXDKBridge.m
+//  Mobile-XDK-Fiuu_Swift
+//
+//  Created by Siti Norathirah Yahya on 21/05/2024.
+//
+
+#ifndef MOLPayXDKBridge_h
+#define MOLPayXDKBridge_h
+
+
+#endif /* MOLPayXDKBridge_h */
+
+#import <MOLPayXDK/MOLPayLib.h>
---
+++ Mobile-XDK-Fiuu_Swift/Mobile-XDK-Fiuu_Swift-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+#ifndef MOLPayXDKBridge_h
+#define MOLPayXDKBridge_h
+
+
+#endif /* MOLPayXDKBridge_h */
+
+#import <MOLPayXDK/MOLPayLib.h>
